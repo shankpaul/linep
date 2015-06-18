69f60e87547d2e338017f4b9aff5ceac532f6a3a
Shan K Paul <shanpaul06@gmail.com>
2015-06-18 17:36:52 +0530

    package changed

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ var express = require('express'),
 app = express(),
 server = require('http').createServer(app),
 io = require('socket.io').listen(server),
-graph = require('fbgraph');
+FB = require('fb');
 
 var conf = {
 	"client_id":      '813365648728696',
 	"client_secret":  'd1698fb5172c726fdbb7b4642d90f2ba',
-	"redirect_uri": 'http://localhost:3001/auth/'
+	"scope":          'email',
+	"redirect_uri": 'http://localhost:3001/auth'
 }
 
+FB.options({
+    appId:          '813365648728696',
+    appSecret:      'd1698fb5172c726fdbb7b4642d90f2ba',
+    redirectUri:    'http://localhost:3001/auth'
+});
 
 server.listen(3001);
 
@@ -19,35 +25,16 @@ server.listen(3001);
 
 //Root location
 app.get('/',function(req,res){
-
-		  // get authorization url
-		  var authUrl = graph.getOauthUrl({
-		  	"client_id":     conf.client_id
-		  	, "redirect_uri":  conf.redirect_uri
-		  });
-  
-  			res.redirect(authUrl); // shows dialog
-		 
-		  
-    // res.sendfile(__dirname + '/index.html');
+ 
+      res.redirect(FB.getLoginUrl({ scope: 'user_about_me' }));       
+   
+   		// res.sendfile(__dirname + '/index.html');
   });
 
 app.get('/auth',function(req,res){
 
-
-	graph.authorize({
-		  	"client_id":      conf.client_id
-		  	, "redirect_uri":   conf.redirect_uri
-		  	, "client_secret":  conf.client_secret
-		  	, "code":           req.query.code
-		  }, function (err, facebookRes) {
-		      // res.redirect('/loggedIn');
-		    });
-		  graph.get("/me", function(err, res) {
-		  	console.log("asa");
-		  console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
-		});
-
+   var code            = req.query.code;
+   console.log(code);
 
 })
 
